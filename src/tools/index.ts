95ccec34d887be524ec4d1d7398f8dbecba1f245
JiangQiang <jq1257403419@vip.qq.com>
2026-07-02 22:54:41 +0800

    feat(tools): 注册 ae:libreoffice 工具和命令模型路由
    将 aeLibreofficeTool 注册到工具注册表，添加 COMMAND.LIBREOFFICE
    到 MODEL_SCENARIO.QUICK 路由，完成 ae:libreoffice 技能的最后拼图。

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -10,6 +10,7 @@ import { aeHelpTool } from './ae-help.tool.js'
 import { aeReviewProofTool } from './ae-review-proof.tool.js'
 import { aeSwaggerParserTool } from './ae-swagger-parser.tool.js'
 import { aeHtmlBundleTool } from './ae-html-bundle.tool.js'
+import { aeLibreofficeTool } from './ae-libreoffice.tool.js'
 import { aeImageTool } from './ae-image.tool.js'
 import { aeGraphBuildTool } from './ae-graph-build.tool.js'
 import { aeGraphQueryTool } from './ae-graph-query.tool.js'
@@ -37,6 +38,7 @@ export function createToolRegistry(): Record<string, ToolDefinition> {
     [TOOL.AE_REVIEW_PROOF]: aeReviewProofTool,
     [TOOL.AE_SWAGGER_PARSER]: aeSwaggerParserTool,
     [TOOL.AE_HTML_BUNDLE]: aeHtmlBundleTool,
+    [TOOL.AE_LIBREOFFICE]: aeLibreofficeTool,
 
     [TOOL.AE_IMAGE]: aeImageTool,
     [TOOL.AE_GRAPH_BUILD]: aeGraphBuildTool,
